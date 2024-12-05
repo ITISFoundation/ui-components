0d9b3351ece1ec4a0ef346b94c34899fe1e1a441
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2024-12-05 13:03:18 +0100

    added missing files

---
+++ packages/docs/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Box } from '@ui-components/workbench'
+
+const App = () => {
+  return <Box title='Amigo'/>
+}
+
+export default App
---
+++ packages/docs/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+const containerElement = document.getElementById('root')
+
+if (containerElement) {
+  const root = createRoot(containerElement);
+  root.render(<App/>);
+}
